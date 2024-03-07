d15cb8b5a2e8d3d866f50f426800fad6c34dfa1a
ssunkyo <psk910903@gmail.com>
2024-03-07 09:11:32 +0900

    Router 소개와 설치 방법

---
+++ learn-vue3/src/Vue Router/1_Setup/1_Introduction.vue
@@ -0,0 +1,30 @@
+<template>
+	<div></div>
+</template>
+<!--  
+
+  소개
+
+    Vue Router는 Vue.js의 공식 라우터이다. Vue.js 코어와 긴밀하게 통합되어 싱글 페이지 앱을 쉽게 만들 수 있다.
+    기능은 다음과 같다.
+
+    - 중첩된 라우트 매핑
+    - 동적 라우팅
+    - 모듈식 컴포넌트 기반의 라우터 설정
+    - 라우트에 파라미터, 쿼리, 와일드카드 적용
+    - Vue.js의 트랜지션 시스템을 사용한 트랜지션 효과
+    - 라우트 탐색을 세밀하게 컨드롤
+    - 링크에 자동으로 액티브 CSS 클래스 적용
+    - HTML5 히스토리 모드 또는 해시 모드
+    - 커스텀 스크롤 동작
+    - URL에 대한 적절한 인코딩
+-->
+<script>
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn-vue3/src/Vue Router/1_Setup/2_Installation.vue
@@ -0,0 +1,58 @@
+<template>
+	<div></div>
+</template>
+<!--  
+
+  설치
+
+    직접 다운로드 / CDN
+
+    https://unpkg.com/vue-router@4
+
+    Unpgk.com은 npm 기반의 CDN 링크를 제공한다. 위 링크는 npm에서 항상 최신 릴리스를 가리킨다.
+    https://unpkg.com/vue-router@4.0.15/dist/vue-router.global.js 와 같은 URL을 통해 특정 버전/태그를 사용할 수도 있다.
+
+    이것은 전역 VueRouter 객체를 통해 Vue Router를 노출시킨다.
+    예를 들어 VueRouter.createRouter(...)와 같이 사용한다.
+
+  -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  패키지 관리자
+
+    JavaScript 패키지 관리자를 사용하는 기존 프로젝트가 있는 경우, npm 레지스트리에서 Vue Router를 설치할 수 있다.
+
+    npm
+      npm install vue-router@4
+
+    yarn
+      yarn add vue-router@4
+
+    pnpm
+      pnpm add vue-router@4
+
+    새 프로젝트를 시작하는 경우, Vue Router를 포함한 옵션으로 Vite 기반 프로젝트를 생성하는 create-vue 스캐폴딩 도구를 사용하는 것이 더 쉬울 수 있다.
+
+    npm
+      npm create vue@latest
+
+    yarn
+      yarn create vue
+
+    pnpm
+      pnpm create vue
+
+    생성하려는 프로젝트의 종류에 대한 몇 가지 질문이 나타난다.
+    Vue Router를 설치하기로 선택하면, 예제 애플리케이션은 Vue Router의 핵심 기능 중 일부를 시연할 것이다.
+
+    패키지 관리자를 사용하는 프로젝트는 일반적으로, ES 모듈을 사용하여 Vue Router에 접근한다.
+    예를 들어, import { createRouter } from 'vue-router'와 같이 사용한다.
+-->
+<script>
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
