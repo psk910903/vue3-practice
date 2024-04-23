a4a172d792d8a6d5ed119dbfa6a6c85eb6960cad
ssunkyo <psk910903@gmail.com>
2024-04-23 10:18:16 +0900

    Typescript - 작성중

--- learn-vue3/src/TypeScript/8_Classes.vue
+++ learn-vue3/src/TypeScript/8_Classes.vue
@@ -471,7 +471,246 @@
 
 				즉, 기본 클래스 생성자는 자신의 생성자 동안 자신의 name 값을 본 것이다. 왜냐하면 파생된 클래스 필드 초기화가 아직 실행되지 않았기 때문이다.
 
-				
+
+			기본 제공 유형 상속(Inheriting Built-in Types)
+
+				참고: Array, Error, Map 등의 내장형에서 상속할 계획이 없거나 컴파일 대상이 ES6/ES2015 이상으로 명시적으로 설정되어 있는 경우 이 섹션을 건너뛸 수 있다.
+
+				ES2015에서 객체를 반환하는 생성자 super(...)의 호출자를 암묵적으로 이 값으로 대체한다.
+				생성된 생성자 코드는 super(...)의 잠재적인 반환 값을 캡처하여 이 값으로 대체하는 것이 필요하다.
+
+				따라서 Error, Array 등을 하위 클래스화하는 것이 더 이상 예상대로 작동하지 않을 수 있다.
+				Error, Array 등에 대한 생성자 함수가 ECMAScript 6의 new.target을 사용하여 프로토타입 체인을 조정하기 때문이다.
+				그러나 ECMAScript 5에서 생성자를 호출할 때 new.target에 대한 값을 보장할 방법이 없다. 다른 다운레벨 컴파일러도 기본적으로 동일한 제한이 있다.
+
+				다음과 같은 하위 클래스의 경우
+
+					class MsgError extends Error {
+						constructor(m: string) {
+							super(m);
+						}
+						sayHello() {
+							return "hello " + this.message;
+						}
+					}
+
+				여기에는 두 가지 문제가 있을 수 있다.
+
+					1 이러한 하위 클래스를 구성하는 객체의 경우 메서드가 정의되지 않을 수 있으므로 sayHello를 호출하면 오류가 발생할 수 있다.
+					2 하위 클래스와 해당 인스턴스 간에 instanceof가 제대로 작동하지 않을 수 있으므로(new MsgError()) instanceof MsgError 가 false를 반환할 수 있다.
+
+				권장사항으로, super(...) 호출 후에는 수동으로 프로토타입을 즉시 조정하는 것이 좋다.
+
+				MsgError의 하위 클래스는 또한 프로토타입을 수동으로 설정해야 한다. Object.setPrototypeOf를 지원하지 않는 런타임의 경우 __proto__를 대신 사용할 수도 있다.
+				그러나 이러한 해결책은 Internet Explorer 10 및 이전 버전에서는 작동하지 않는다. 프로토타입 체인 자체를 수정할 수는 없지만, 프로토타입에서 메서드를 수동으로 인스턴스로 복사할 수 있다.
+				(this에서 MsgError.prototype으로)
+
+
+		멤버 가시성(Member Visibility)
+
+			TypeScript를 사용하여 클래스 외부 코드에서 특정 메서드나 속성이 보여지는 여부를 제어할 수 있다.
+
+
+			public
+
+				클래스 멤버의 기본 가시성은 public이다. public 멤버는 어디에서든 엑세스할 수 있다.
+
+					class Greeter {
+						public greet() {
+							console.log("hi!");
+						}
+					}
+					const g = new Greeter();
+					g.greet();
+
+				public은 이미 기본 가시성 한정자이기 때문에 클래스 멤버에 대해 이를 작성할 필요는 없지만, 스타일이나 가독성을 위해 작성할 수도 있다.
+
+
+			protected
+
+				protected 멤버는 선언된 클래스의 하위 클래스에서만 볼 수 있다.
+
+					class Greeter {
+						public greet() {
+							console.log("Hello, " + this.getName());
+						}
+						protected getName() {
+							return "hi";
+						}
+					}
+
+					class SpecialGreeter extends Greeter {
+						public howdy() {
+							// 여기서 보호된 멤버에 엑세스한다.
+							console.log("Howdy, " + this.getName());
+						}
+					}
+
+					const g = new SpecialGreeter();
+					g.greet(); // OK
+					g.getName();
+					Property 'getName' is protected and only accessible within class 'Greeter' and its subclasses.
+
+
+				보호되는 멤버의 노출(Exposure of protected members)
+
+					하위 클래스는 기본 클래스 계약을 따라야 하지만, 더 많은 기능을 갖춘 기본 클래스의 하위 유형을 노출시킬 수도 있다.
+					이는 protected 멤버를 public으로 공개하는 것을 포함한다.
+
+						class Base {
+							protected m = 10;
+						}
+						class Derived extends Base {
+							// 수식어가 없으므로 기본값은 public이다.
+							m = 15;
+						}
+						const d = new Derived();
+						console.log(d.m); // OK
+
+					Derived가 이미 m을 자유롭게 읽고 쓸 수 있었기 때문에 이 작업은 상황의 "보안"을 의미적으로 변경하지 않는다.
+					여기서 주목해야 할 주요 사항은 파생된 클래스에서 이러한 노출이 의도적이지 않은 경우 protected 수정자를 반복해서 사용해야한다는 것이다.
+
+
+				계층 간 보호된 엑세스(Cross-hierarchy protected access)
+
+					다른 OOP언어는 기본 클래스 참조를 통해 protected 멤버에 엑세스 하는 것이 합법적인지에 대해 의견이 분분하다.
+
+						class Base {
+							protected x: number = 1;
+						}
+						class Derived1 extends Base {
+							protected x: number = 5;
+						}
+						class Derived2 extends Base {
+							f1(other: Derived2) {
+								other.x = 10;
+							}
+							f2(other: Derived2) {
+								other.x = 10;
+						Property 'x' is protected and only accessible within class 'Derived1' and its subclasses.
+							}
+						}
+						
+					예를 들어 Java는 이를 합법적인 것으로 간주한다. 반면에 C# 및 C++은 이 코드가 불법적이어야 한다고 선택했다.
+
+					TypeScript는 여기서 C# 및 C++과 같은 입장을 취하는데, Derived에서 x에 엑세스하는 것은 Derived2의 하위 클래스에서만 허용되어야 하며, Derived1은 하위 클래스가 아니다.
+					게다가 Derived1 참조를 통해 x에 엑세스 하는 것이 불법적이라면 (확실히 그렇게여야 한다) 기본 클래스 참조를 통해 엑세스 하는 것이 상황을 개선시키지 않아야 한다.
+
+
+			private
+
+				private는 protected와 비슷하지만 하위 클래스에서도 멤버에 엑세스할 수 없다.
+
+					class Base {
+						private x = 0;
+					}
+					const b = new Base();
+					// 클래스 외부에서 액세스할 수 없음
+					console.log(b.x);
+					Property 'x' is private and only accessible within class 'Base'.
+
+					class Derived extends Base {
+						showX() {
+							// 하위 클래스에서 액세스할 수 없음
+							console.log(this.x);
+					Propterty 'x' is private and only accessible within class 'Base'.
+						}
+					}
+
+				private 멤버는 파생된 클래스에서 보이지 않기 때문에 파생된 클래스에서 가시성을 높일 수 없다.
+
+					class Base {
+						private x = 0;
+					}
+					class Derived extends Base {
+					Class 'Derived' incorrectly extends base class 'Base'.
+						Property 'x' is private in type 'Base' but not in type 'Derived'.
+						x = 1;
+					}
+
+				Cross-instance private access
+
+					다양한 객체지향 프로그래밍 언어들은 동일한 클래스의 다른 인스턴스가 서로의 private 멤버에 접근할지 여부에 대해 의견이 분분하다.
+					Java, C#, C++, Swift 및 PHP와 같은 언어들은 이를 허용하지만 Ruby는 허용하지 않는다.
+
+					TypeScript는 인스턴스 간에 private 멤버에 접근할 수 있다.
+
+						class A {
+							private x = 10;
+
+							public sameAs(other: A) {
+								// No error
+								return other.x === this.x;
+							}
+						}
+
+				주의사항
+
+					타입스크립트의 타입 시스템의 다른 측면들과 마찬가지로, private와 protected는 타입 체크 중에만 강제된다.
+
+					이는 JavaScript 런타임 구조인 in 이나 간단한 속성 조회와 같은 것들이 여전히 private나 protected 멤버에 접근할 수 있다는 것을 의미한다.
+
+						class MySafe {
+							private secretKey = 12345;
+						}
+
+						// 자바스크립트 파일에서
+						const s = new MySafe();
+						// 12345를 출력할 것이다.
+						console.log(s.secretKey);
+
+					private 키워드를 사용한 경우, 타입 체크 중에도 대괄호 표기법을 사용하여 접근할 수 있다.
+					이렇게 함으로써 private로 선언된 필드를 유닛 테스트와 같은 작업에서 더 쉽게 접근할 수 있게 된다.
+					그러나 이러한 방법은 필드가 소프트한 private가 되어 엄격한 privacy를 강제하지 않는다는 단점이 있다.
+
+						class MySafe {
+							private secretKey = 12345;
+						}
+
+						const s = new MySafe();
+
+						// 유형 확인 중에는 허용되지 않음
+						console.log(s.secretKey);
+						Property 'secretKey' is private and only accessible within class 'MySafe'.
+
+						// OK
+						console.log(s["secretKey"]);
+
+					TypeScript의 private와는 달리 JavaScript의 private 필드(#)는 컴파일 후에도 계속해서 private 상태를 유지하며, 이전에 언급한 대괄호 표기법 접근과 같은 탈출구를 제공하지 않는다.
+					이러한 특성으로 인해 JavaScript의 private 필드는 엄격한 privacy를 제공하는 'hard private'로 볼 수 있다.
+
+						class Dog {
+							#barkAmount = 0;
+							personality = "happy";
+
+							constructor() {}
+						}
+
+						"use strict";
+						class Dog {
+							#barkAmount = 0;
+							personality = "happy";
+							constuctor() { }
+						}
+
+					ES2021 이하로 컴파일할 때 TypeScript는 # 대신 WeakMaps을 사용한다.
+
+						"use strict"
+						var _Dog_barkAmount;
+						class Dog {
+							constructor() {
+								_Dog_barkAmount.set(this, 0);
+								this.personality = "happy";
+							}
+						}
+						_Dog_barkAmount = new WeakMap();
+
+					악의적인 사용자로부터 클래스 내의 값을 보호해야 하는 경우, 클로저, WeakMaps 또는 private 필드와 같은 런타임에서 엄격한 개인 정보 보호를 제공하는 매커니즘을 사용해야 한다.
+					런타임에서 추가된 개인 정보 보호 검사는 성능에 영향을 줄 수 있다.
+
+
+			Static Members
+
 -->
 <script>
 export default {
